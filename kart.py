ff2bdf05edee68fbb911868a68b10e1ca7e557c2
kartart22 <karenartunduaga22@gmail.com>
2026-03-03 11:35:19 -0500

    FIGURAS GEOMETRICAS
    menú de figuras geométricas

---
+++ kart.py
@@ -0,0 +1,28 @@
+print("FIGURAS GEOMÉTRICAS")
+print("1. Cuadrado")
+print("2. Rectángulo")
+print("3. Círculo")
+print("4. Triángulo")
+
+try:
+    opcion = int(input("Elige una opción: "))
+
+    if opcion == 1:
+        print("\nHas elegido hacer un CUADRADO")
+
+    elif opcion == 2:
+        print("\nHas elegido hacer un RECTÁNGULO")
+  
+    elif opcion == 3:
+        print("\nHas elegido hacer un CÍRCULO")
+   
+
+    elif opcion == 4:
+        print("\nHas elegido hacer un TRIÁNGULO")
+   
+
+    else:
+        print("Seleccione un número válido")
+
+except ValueError:
+    print("Debe ingresar un número válido")
