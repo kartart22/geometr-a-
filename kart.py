efd8094bb7a24a9f8ec82ad0e1f27af20154a35f
meilingserrato-gif <meilingserrato@gmail.com>
2026-03-09 11:05:53 -0500

    kart.py
    figuras 2d y 3d karen julieth artunduaga cardenas

--- kart.py
+++ kart.py
@@ -1,24 +1,33 @@
-print("FIGURAS GEOMÉTRICAS")
-print("1. estrella")
-print("2. Rectángulo")
-print("3. Círculo")
-print("4. Triángulo")
-
-try:
-    opcion = int(input("Elige una opción: "))
-
+def hacer_estrella():
+    print("haciendo una estrella")
+def hacer_rectangulo():
+    print("haciendo un rectangulo")
+def hacer_circulo():
+    print("haciendo un circulo")
+def hacer_triangulo():
+    print("haciendo un triangulo")
+def menu():
+    print("FIGURAS GEOMÉTRICAS")
+    print("1. estrella")
+    print("2. Rectángulo")
+    print("3. Círculo")
+    print("4. Triángulo")
+while True:
+    menu()
+    try:
+        opcion = int(input("Elige una opción: \n"))
     if opcion == 1:
         print("\nHas elegido hacer un estrella")
 
     elif opcion == 2:
-        print("\nHas elegido hacer un RECTÁNGULO")
+        print("\nHas elegido hacer un rectangulo")
   
     elif opcion == 3:
-        print("\nHas elegido hacer un CÍRCULO")
+        print("\nHas elegido hacer un circulo")
    
 
     elif opcion == 4:
-        print("\nHas elegido hacer un TRIÁNGULO")
+        print("\nHas elegido hacer un triangulo")
    
 
     else:
@@ -26,3 +35,4 @@ try:
 
 except ValueError:
     print("Debe ingresar un número válido")
+
